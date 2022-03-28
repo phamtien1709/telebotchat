e8fc799d7e4277f87e0ebf2319c7d7851aece473
Tien Pham <phamtien1709@gmail.com>
2022-03-28 21:17:33 +0700

    First commit

---
+++ controller/CommonController.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const axios = require('axios')
+const dayjs = require('dayjs')
+const InterestedSymbols = require('../lib/interestedSymbols')
+
+const { TELEGRAM_TOKEN } = process.env
+const TELEGRAM_API = `https://api.telegram.org/bot${TELEGRAM_TOKEN}`
+
+class CommonController {
+  async getPrice(message) {
+    const responsePrice = await axios.get(`https://api.binance.com/api/v3/ticker/24hr`)
+    const interestedResponse = responsePrice.data.filter((symbol) =>
+      InterestedSymbols.MOSTLY.includes(symbol.symbol),
+    )
+    let newMessage = ''
+    newMessage += `Cap nhat gia coin dang quan tam(24h) (${dayjs().format('YYYY-MM-DD HH:mm:ss')}):\n`
+    for (let i = 0; i < interestedResponse.length; i++) {
+      newMessage += `\xF0\x9F\x92\xB0 ${interestedResponse[i].symbol}: \xF0\x9F\x92\xB2${interestedResponse[
+        i
+      ].lastPrice.slice(0, -3)}. ${
+        interestedResponse[i].priceChangePercent.includes('-')
+          ? '\xF0\x9F\x93\x89'
+          : '\xF0\x9F\x93\x88'
+      }: ${interestedResponse[i].priceChangePercent.slice(0, -1)}%\n`
+    }
+    const responseSender = await axios.get(
+      `${TELEGRAM_API}/sendMessage?chat_id=${message.chat.id}&text=${newMessage}`,
+    )
+  }
+}
+
+module.exports = new CommonController()
---
+++ index.js
@@ -0,0 +1,55 @@
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const axios = require('axios')
+const schedule = require('node-schedule')
+const TrainingKeys = require('./lib/trainingKeys')
+const CommonController = require('./controller/CommonController')
+
+let job
+
+const { TELEGRAM_TOKEN, SERVER_URL } = process.env
+const TELEGRAM_API = `https://api.telegram.org/bot${TELEGRAM_TOKEN}`
+const URI = `/webhook/${TELEGRAM_TOKEN}`
+const WEBHOOK_URL = SERVER_URL + URI
+
+const app = express()
+app.use(bodyParser.json())
+
+const init = async () => {
+  const response = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`)
+  console.log(response.data)
+}
+
+app.post(URI, async (req, res) => {
+  const { message } = req.body
+  if (message.text === TrainingKeys.COMMON_START) {
+    const response = await axios.get(
+      `${TELEGRAM_API}/sendMessage?chat_id=${message.chat.id}&text=${message.text}`,
+    )
+  }
+  if (message.text === TrainingKeys.COMMON_PRICE) {
+    await CommonController.getPrice(message)
+  }
+  if (message.text === TrainingKeys.COMMON_PRICE_1H) {
+    job = schedule.scheduleJob('30 * * * * ', async () => {
+      await CommonController.getPrice(message)
+    })
+  }
+  if (message.text === TrainingKeys.COMMON_STOP_PRICE_1H) {
+    if (job) {
+      job.cancel()
+    }
+  }
+  if (message.text === TrainingKeys.COMMON_STOP) {
+    const response = await axios.get(
+      `${TELEGRAM_API}/sendMessage?chat_id=${message.chat.id}&text=${message.text}`,
+    )
+  }
+  res.sendStatus(200)
+})
+
+app.listen(process.env.PORT || 5000, async () => {
+  console.log('Server started', process.env.PORT || 5000)
+  await init()
+})
