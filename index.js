adfe9d68b79f5b439c254f63d8454c2efd3db196
Titus Pham <phamtien1709@gmail.com>
2024-03-14 17:32:55 +0700

    Update .gitignore and package.json, add Telegram bot functionality

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const axios = require('axios')
 const schedule = require('node-schedule')
 const TrainingKeys = require('./lib/trainingKeys')
 const CommonController = require('./controller/CommonController')
+const { Telegraf } = require('telegraf')
+const { message } = require('telegraf/filters')
 
 let job
 
@@ -12,13 +14,14 @@ const { TELEGRAM_TOKEN, SERVER_URL } = process.env
 const TELEGRAM_API = `https://api.telegram.org/bot${TELEGRAM_TOKEN}`
 const URI = `/webhook/${TELEGRAM_TOKEN}`
 const WEBHOOK_URL = SERVER_URL + URI
+const bot = new Telegraf(TELEGRAM_TOKEN)
 
 const app = express()
 app.use(bodyParser.json())
 
 const init = async () => {
-  const response = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`)
-  console.log(response.data)
+  // const response = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`)
+  // console.log(response.data)
 }
 
 app.post(URI, async (req, res) => {
@@ -57,7 +60,7 @@ app.post(URI, async (req, res) => {
     }
     res.sendStatus(200)
   } catch (error) {
-		console.log('ERRORORO');
+    console.log('ERRORORO')
     res.sendStatus(200)
   }
 })
@@ -76,3 +79,42 @@ app.listen(process.env.PORT || 5000, async () => {
   console.log('Server started', process.env.PORT || 5000)
   await init()
 })
+
+bot.start((ctx) =>
+  ctx
+    .reply('Welcome', {
+      reply_to_message_id: process.env.TOPIC_ID,
+    })
+    .catch((error) => {}),
+)
+bot.help((ctx) =>
+  ctx
+    .reply('Send me a sticker', {
+      reply_to_message_id: process.env.TOPIC_ID,
+    })
+    .catch((error) => {}),
+)
+
+bot.on(message('sticker'), (ctx) =>
+  ctx.reply(['🖐', '🖖', '🤏', '🤞', '🤟', '👍'][Math.floor(Math.random() * 6)], {
+    reply_to_message_id: process.env.TOPIC_ID,
+  }),
+)
+bot.hears('Hi', (ctx) => {
+  ctx.telegram
+    .sendMessage(ctx.message.chat.id, 'OK', {
+      reply_to_message_id: process.env.TOPIC_ID,
+    })
+    .catch((error) => {})
+})
+bot.hears('ID', (ctx) => {
+  try {
+    ctx.telegram
+      .sendMessage(ctx.message.chat.id, ctx.message.chat.id.toString(), {
+        reply_to_message_id: process.env.TOPIC_ID,
+      })
+      .catch((error) => {})
+  } catch (error) {}
+})
+
+bot.launch()
